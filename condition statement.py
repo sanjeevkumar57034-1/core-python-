962c16e176cd3eef6b1bacb01a43652c4a4b7ae2
sanjeev <sanjeevkumar57034@gmail.comgit config --global user.email sanjeevkumar57034@gmail.com>
2026-08-21 21:15:11 +0530

    intial commit

---
+++ condition statement.py
@@ -0,0 +1,48 @@
+#if else
+#elif statement
+# #check value is (pos,neg,zero)
+# x=7.8
+# if x>0:
+#     print("positive value=",x)
+# elif x<0:
+#     print("negative value=",x)
+# else:
+#     print("zero value=",x)
+import numbers
+
+#check num is add even
+# for i in range(21):
+#     if i % 2== 0:
+#         print("even number",i)
+#     elif i%2!=0:
+#         print("odd number",i)
+
+#check value is (positive , negetive , zero )
+
+# n=int(input("enter the number="))
+# if n>0:
+#     print("positive number=",n)
+# elif n<0:
+#     print("negative number=",n)
+# else:
+#     print("zero number=",n)
+
+
+#user check value ( even , odd , )
+
+# n=int(input("enter the number="))
+# even_count = 0
+# od        elif i % 2 != 0:
+#         print(i, "=odd")
+#         odd_count += 1
+#     else:
+#         print(i, "=zero")
+# print("total even number=", even_count)
+# print("total odd number=", odd_count)
+# d_count = 0
+# for i in range(1,n+1):
+#     if i%2==0:
+#         print(i,"=even")
+# even_count+=1
+
+
