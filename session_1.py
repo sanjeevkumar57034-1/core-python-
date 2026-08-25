499e0b36599f7d32fb939f437b53a67c71603b0e
sanjeev <sanjeevkumar57034@gmail.comgit config --global user.email sanjeevkumar57034@gmail.com>
2026-08-25 21:12:47 +0530

    add session 1 2 3

--- session_1.py
+++ session_1.py
@@ -1,3 +1,17 @@
 #task 2
 
 print("hello, instagram world!")
+
+#task_3
+print("\n1,instagram")
+print("python can be use backend service,and data anlysis")
+print("\n2,whatsaap")
+print("python can be use backend service,and data anlysis")
+print("\n3,facebook")
+print("python can be use backend service,and data anlysis")
+
+#task_4
+import sys
+print("hello,intagram world!")
+print("python version")
+print("sys.version")
---
+++ session_2.py
@@ -0,0 +1,62 @@
+#task_1 flipkart product
+product_name="smart watch"
+price=20000
+is_availble=True
+ratting=8.5
+print("product_name:",product_name)
+print("data type:",type(product_name))
+print("price:",price)
+print("data type:",type(price))
+print("is_availble:",is_availble)
+print("data type:",type(is_availble))
+print("ratting:",ratting)
+print("data type:",type(ratting))
+
+#task_2 zomato restaurent
+
+cuisine_type="indian"
+average_cost=200
+is_open_now=True
+print("\nrestaurent_name:")
+print("restaurant_name:",cuisine_type)
+print("average_cost:",average_cost)
+print("is_open_now:",is_open_now)
+
+#task_3 spotyfy_playlist
+
+playlist_litle="my playlist"
+number_of_song=10
+is_public=False
+print("\nplaylist_litle:")
+print("playlist_litle:",playlist_litle)
+print("ID:",id(playlist_litle))
+print("number_of_song:",number_of_song)
+print("ID:",id(number_of_song))
+print("is_public:",is_public)
+print("ID:",id(is_public))
+
+#task_4 youtube
+
+title="python assignment"
+views= 200000
+like=100000
+is_premium=False
+print("title:",title)
+print("data type:",type(title))
+print("views:",views)
+print("data type:",type(views))
+print("like:",like)
+print("data type:",type(like))
+print("is_premium:",is_premium)
+print("data type:",type(is_premium))
+
+# title is a string,views and likes are integers,and is_primium is a boolean.
+# These are basic data types that store single values.
+#lists,tuples,sets,and dictionaries can store multiple value.
+
+
+
+
+
+
+
